93ddd52f2fb924456d969c6d1d8b8113af42e550
Edi <edipajanen@gmail.com>
2026-08-27 11:58:56 +0300

    Leikattiin Sam-Handwich.py:ta.

--- mod03/Sam-Handwich.py
+++ mod03/Sam-Handwich.py
@@ -10,6 +10,3 @@ fahrenheit = input("Anna lämpötila fahrenheitteina\nVastaus:")
 celsius = (int(fahrenheit) - 32) * 5 / 9
 # print (fahrenheit + " fahrenheittia ynnää " + str(celsius) + " celsiusta.")
 print(f"Lämpötila fahrenheitteina: {int(fahrenheit):6.2f} Lämpötila celsiuksina: {celsius:6.2f}")
-
-import math
-print ((f{math.pi):6.2f})
