1eb3504478fb2078abff4c89639696a4d32e5911
Edi <edipajanen@gmail.com>
2026-08-27 11:47:03 +0300

    Ensimmäinen tuntitehtävä, Pinta-alat,py, tehty. Kriittinen Troll Face lisätty.

--- mod03/Sam-Handwich.py
+++ mod03/Sam-Handwich.py
@@ -1,9 +1,15 @@
 # Muista imuroida lattiat.
 # Ctrl + K + S
 # cls terminaalissa tyhjentää sen.
+# ctrl + k + c = commentoi
+# ctrl + k + u = uncommentoi
 
 # Tässä on teikäläiselle ohjelma, joka kysyy käyttäjältä fahrenheitin ja antaa celsiuksen:
 print ("Helou. Jos on vaikeuksia muuntaa fahrenheit-asteita celsiusasteiksi, olet tullut oikeaan paikkaan.")
 fahrenheit = input("Anna lämpötila fahrenheitteina\nVastaus:")
 celsius = (int(fahrenheit) - 32) * 5 / 9
-print (fahrenheit + " fahrenheittia ynnää " + str(celsius) + " celsiusta.")
+# print (fahrenheit + " fahrenheittia ynnää " + str(celsius) + " celsiusta.")
+print(f"Lämpötila fahrenheitteina: {int(fahrenheit):6.2f} Lämpötila celsiuksina: {celsius:6.2f}")
+
+import math
+print ((f{math.pi):6.2f})
